77178eb2571e745023a70f38dc9f3d7d7503abad
Dario Niero <dario.niero@enginius.com>
2023-08-14 18:47:45 +0200

    WIP: add id to x and y axis

--- js/main.js
+++ js/main.js
@@ -1166,12 +1166,14 @@ const svg = d3.create("svg").attr("width", width).attr("height", height);
 svg
   .append("g")
   .attr("transform", `translate(0,${height - marginBottom})`)
+  .attr("id", "x-axis")
   .call(d3.axisBottom(x));
 
 // Add the y-axis.
 svg
   .append("g")
   .attr("transform", `translate(${marginLeft},0)`)
+  .attr("id", "y-axis")
   .call(d3.axisLeft(y));
 
 
